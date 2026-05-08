181b1e0190333b6d86395eaacd862232a528d013
Haruto Jaulin <jaulinex+dev@gmail.com>
2026-05-07 19:10:27 -0500

    Created mixin class structure and added it to the mixins json

---
+++ src/main/java/com/jaulinex/brokenbadplus/mixin/MethItemMixin.java
@@ -0,0 +1,8 @@
+package com.jaulinex.brokenbadplus.mixin;
+
+import com.jetpacker06.CreateBrokenBad.item.MethItem;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MethItem.class)
+abstract class MethItemMixin {
+}
